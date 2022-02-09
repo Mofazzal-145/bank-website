8eca31aecf90c3fcc204338d6d9c3df576d811bf
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-10 02:07:45 +0600

    banking file added

---
+++ js/bank.js
@@ -0,0 +1,20 @@
+document.getElementById('longin-button').addEventListener('click', function(){
+
+    // get user email
+
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+
+
+    // get user password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    if(userEmail == 'mofu@gmail.com' && userPassword == 'mofu'){
+        window.location.href = 'banking.html';
+    }
+
+
+})
